357df05b1032ab538ab5077f17da6e077e1a5926
Annkkitaaa <ankitasingh15.102@gmail.com>
2025-03-30 12:56:03 +0530

    created server.js

---
+++ backend/src/api/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const cors = require('cors');
+const routes = require('./routes');
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.use('/api', routes);
+
+// Root route
+app.get('/', (req, res) => {
+  res.json({ message: 'DeFi Advisor API is running' });
+});
+
+// Error handling
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ 
+    success: false, 
+    error: 'Server error' 
+  });
+});
+
+module.exports = app;
